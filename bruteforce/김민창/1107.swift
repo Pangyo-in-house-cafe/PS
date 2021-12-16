ae4047b837bb8ed5c51e000aed3dfd8677641175
MinChang <rud0806@gmail.com>
2021-12-16 19:48:41 +0900

    민창: 1107

---
+++ bruteforce/김민창/1107.swift
@@ -0,0 +1,37 @@
+//
+//  main.swift
+//  Swift-PS
+//
+//  Created by 김민창 on 2021/07/24.
+//
+
+import Foundation
+
+func main() {
+    let n = Int(readLine()!)!
+    let m = Int(readLine()!)!
+    let button = readLine()!.split(separator: " ").map { Int($0)! }
+    
+    var breakdown = Array(repeating: false, count: 11)
+    for i in button { breakdown[i] = true }
+    
+    var answer = abs(n - 100)
+    
+    for i in 0...999999 {
+        let num = String(i).map { $0.wholeNumberValue! }
+        let count = num.count
+        var flag = false
+        for j in 0..<count {
+            if breakdown[num[j]] {
+                flag = true
+                break
+            }
+        }
+        
+        if !flag { answer = min(answer, abs(n - i) + count) }
+    }
+    print(answer)
+}
+
+main()
+
